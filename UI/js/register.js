e054d829e0d71c96909805abf6a0bb8a53e81d22
Arrotech <arrotechdesign@gmail.com>
2019-08-13 20:51:20 +0300

    Feature (Fetch Register) User can register
    What does this pr do?
    
    Have a new user able to create a new account.
    
    Any tasks involved?
    
    Fetch register endpoint.
    Create a user interface.
    Style the page with CSS.
    
    Any related pivotal stories?
    
    [Finishes #167877183]

---
+++ UI/js/register.js
@@ -0,0 +1,54 @@
+function callToast() {
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+function onSuccess(msg){
+    document.getElementById('snackbar').innerText = msg
+    callToast();
+}
+function raiseError(msg){
+    document.getElementById('snackbar').innerText = msg
+    callToast();
+}
+
+document.getElementById('register').onclick = () => {
+        event.preventDefault();
+
+        let firstname = document.getElementById('firstname').value;
+        let lastname = document.getElementById('lastname').value;
+        let phone = document.getElementById('phone').value;
+        let username = document.getElementById('username').value;
+        let email = document.getElementById('email').value;
+        let password = document.getElementById('password').value;
+
+        fetch('http://localhost:5000/api/v1/auth/register', {
+            method: 'POST',
+            headers : {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify({firstname:firstname, lastname:lastname, phone:phone, username:username, email:email, password:password})
+        }).then((res) => res.json())
+        .then((data) =>  {
+
+            console.log(data);
+            let user = data['user'];
+            let status = data['status'];
+            let message = data['message'];
+            if (status === '201'){
+                localStorage.setItem("user", JSON.stringify(data[0]));
+                localStorage.setItem('user', data.user);
+                localStorage.setItem('username', data.user.username);
+                localStorage.setItem('email', data.user.email);
+                onSuccess('Account created successfully!');
+                window.location.replace('login.html');
+            }else{
+                raiseError(message);
+            }
+        })
+        .catch((err)=>{
+            raiseError("Please check your internet connection and try again!");
+            console.log(err);
+        })
+    }
